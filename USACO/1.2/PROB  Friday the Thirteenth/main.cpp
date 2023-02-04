dcf641773157ed80cb245bf3a94be3d2f2d363dc
Ibrahim Hisham <muhammadibrahimhisham@gmail.com>
2023-02-04 23:11:19 +0500

    optimize algorithm to stop expensive function call

--- USACO/1.2/PROB  Friday the Thirteenth/main.cpp
+++ USACO/1.2/PROB  Friday the Thirteenth/main.cpp
@@ -20,80 +20,37 @@ bool isLeapYear(int currentYear)
     return false;
 }
 
-int getTotalDays(int years)
-{
-    int days = 0;
-
-    for (int i = 0; i < years; ++i) {
-        if(isLeapYear(1900+i))
-            days += 366;
-        else
-            days += 365;
-    }
-    return days;
-}
-
-int getDate(int days)
-{
-    int date = 1;
-
-    int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int currentMonth = 0;
-
-    int currentYear = 1900;
-
-    for (int i = 0; i < days; ++i) {
-        if(isLeapYear(currentYear))
-            monthDays[1] = 29;
-
-        if(date == monthDays[currentMonth])
-        {
-            date = 1;
-            currentMonth++;
-            if(currentMonth > 11)
-            {
-                currentMonth = 0;
-                currentYear++;
-                date = 1;
-                monthDays[1] = 28;
-            }
-            continue;
-        }
-        date++;
-    }
-
-    // cout << date << " -- " << currentMonth << "\n";
-    return date;
-}
-
 int main()
 {
-    ifstream cin("friday.in");
-
     ofstream friday;
     friday.open("friday.out");
 
     int years;
     cin >> years;
 
-    int days = getTotalDays(years);
+    int curDay = 0;
+    int monthDays[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
     array<int, 7> frequencies = {};
-    int currentDay = 0;
 
-    for (int i = 0; i < days; ++i) {
-       if(currentDay > 6)
-           currentDay = 0;
-
-       if(getDate(i) == 13)
-           frequencies[currentDay] += 1;
-
-        currentDay += 1;
+    for (int year = 1900; year < 1900 + years; ++year) {
+        for (int month = 0; month < 12; ++month) {
+            monthDays[1] = isLeapYear(year) ? 29 : 28;
+            int day = (curDay + 12) % 7;
+            
+            frequencies[day]++;
+            curDay += monthDays[month];
+        }
     }
 
     int order[7] = {5,6,0, 1,2,3,4};
 
     for (int i = 0; i < frequencies.size(); i++) {
+        if(i == frequencies.size() - 1)
+        {
+            friday << frequencies[order[i]] << endl;
+            continue;
+        }
         friday << frequencies[order[i]] << " ";
     }
 
