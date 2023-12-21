66e19d75ef74a483c7fd58e78e5808239706591c
Ibrahim Hisham <muhammadibrahimhisham@gmail.com>
2023-12-21 14:28:49 +0500

    Update increasing_array.cpp

--- CSES/Increasing Array/increasing_array.cpp
+++ CSES/Increasing Array/increasing_array.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
+    // todo: optimize further to learn more about problem
+
     int n = 0;
     cin >> n;
+
+    long long ans = 0;
+    int mn = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+
+        mn = max(x, mn);
+        ans += mn - x;
+    }
+
+    cout << ans << endl;
 }
