ee4d90e60ca4d5364d45faec248a120a0fe633e8
muhammadibrahimhisham@gmail.com <muhammadibrahimhisham@gmail.com>
2023-12-30 02:35:29 +0500

    reorder cses problems

---
+++ CSES/Introductory Problems/Two Knights/two_knights.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ll n;
+    cin >> n;
+
+    for (ll k = 1; k <= n; k++)
+    {
+        if (k == 1)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+    }
+}
