033e232d7c8fdd3200fd406681b685500ccdd67f
Ibrahim Hisham <muhammadibrahimhisham@gmail.com>
2024-01-11 01:06:12 +0500

    techwars q2

--- Misc/Techwars 2024/question_2.cpp
+++ Misc/Techwars 2024/question_2.cpp
@@ -5,4 +5,19 @@ using namespace std;
 
 int main()
 {
+    ll n;
+    cin >> n;
+
+    for (ll i = 0; i < n; i++)
+    {
+        ll coefficient = 1;
+
+        for (int j = 0; j <= i; j++)
+        {
+            cout << " " << coefficient;
+            coefficient = coefficient * (i - j) / (j + 1);
+        }
+
+        cout << endl;
+    }
 }
