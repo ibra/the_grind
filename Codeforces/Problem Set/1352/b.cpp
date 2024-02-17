c8728d65a1497bba704342b1e2f83f9d0b35f3ac
Ibrahim Hisham <muhammadibrahimhisham@gmail.com>
2024-02-18 01:31:56 +0500

    update template

---
+++ Codeforces/Problem Set/1352/b.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+
+int main()
+{
+    ll t;
+    while (t--)
+    {
+        ll n, k;
+        cin >> n >> k;
+
+        if (n % 2 == 0)
+        {
+        }
+        else
+        {
+        }
+    }
+}
